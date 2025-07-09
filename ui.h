dec6114af64b36976a777c31288b176a085921dd
Darpandeep_Kaur <77214264+darpandeepkaur@users.noreply.github.com>
2025-07-08 20:29:01 -0700

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,2 +1,15 @@
-#include <stdio.h>
-using namespace std;
+#pragma once
+
+class ui {
+    public:
+    
+    ui();        //constructor
+    ui(int argc);     //constructor with arguments
+
+
+    void displayMainMenu();
+    void displayCreateMenu();
+    void displayDeleteMenu();
+    void userInterface();
+
+};
