c6fa06e22b868f9469e4d0cf5da4a452e34e4005
Darpandeep_Kaur <77214264+darpandeepkaur@users.noreply.github.com>
2025-07-08 20:28:24 -0700

    Update sailing.h

---
+++ sailing.h
@@ -0,0 +1,18 @@
+#pragma once
+
+using namespace std;
+
+#include <string>
+
+class sailing{
+    private:
+
+
+    public:
+        sailing();
+        sailing();
+
+        bool checkExists(string SailingId);
+        bool writeSailing(string SailingId, string vesselName);
+
+};
