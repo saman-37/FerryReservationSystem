44a95b36b0bf5c2aed4cf8ebfac76acc79c2e3db
Darpandeep_Kaur <77214264+darpandeepkaur@users.noreply.github.com>
2025-07-08 20:27:33 -0700

    Update otherControl.h

---
+++ otherControl.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class otherControl {
+    public:
+        otherControl(); // constructor
+
+        void displayOtherMenu(); // displays the other menu
+        void displayCreateMenu(); // displays the create menu
+        void displayDeleteMenu(); // displays the delete menu
+        void userInterface(); // main user interface for other control
+};
