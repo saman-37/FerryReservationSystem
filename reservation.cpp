ab373d3593fbeba26a067a48cb17fd6735e374c7
Saman <brarsaman38@email.com>
2025-07-23 17:05:59 -0700

    Changed name of the class

--- reservation.cpp
+++ reservation.cpp
@@ -1,107 +1,90 @@
+    
+    
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  // Each .cpp file should begin with this line
 // July 10, 2025 Revision 1     //Each implementation .cpp module will need a version history at the top
-                                // Introductory comment which adds to already provided comment in .h file,describe any overall design issues internal to this module (like data structure (if there are any) and algorithm (e.g. linear search of file
+// Introductory comment which adds to already provided comment in .h file,describe any overall design issues internal to this module (like data structure (if there are any) and algorithm (e.g. linear search of file
 
 //************************************************************
-#include "reservation.h"
+#include "Reservation.h"
+#include "util.h"
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <sstream>
 
-class Reservation {
-    public:
-        // Fixed lengths for binary record layout
-        static const int SAILING_ID_LENGTH = 15;
-        static const int LICENSE_LENGTH = 10;
-        static const int PHONE_LENGTH = 14;
-        static const int RECORD_SIZE = SAILING_ID_LENGTH + LICENSE_LENGTH + PHONE_LENGTH + 1;
+using namespace std;
 
-        // Reservation record fields
-        char sailingId[SAILING_ID_LENGTH + 1]; // +1 for null terminator
-        char license[LICENSE_LENGTH + 1]; // +1 for null terminator
-
-        char phone[PHONE_LENGTH + 1]; // +1 for null terminator
-        //WE DONT NEED TO STORE PHONE IN RESERVATION, LICENSE AND SAILING ID ARE KEYS
-        //but WE MAY NEED IT, IN-CASE THE VEHICLE IS NOT ALREADY, WE WILL CREATE ITS OBJECT AND PUSH IT IN VEHICLE FILE FOR FUTURE easy referance and reservation creation
-
-        bool checkedIn; // Track whether the vehicle has checked in.
-
-        //????????????????????
-        Reservation(); // Default Constructor
-
-        Reservation(const string& sailingId, const string& license, const string& phone) // in: sailingID, license, phone
-        {
-            int length;
-            int height;
-            //search in "vehicle" file, if it already exists
-            //SO HERE, DO I NEED TO HAVE .csv file for vehicle,which i will communicate to now 
-
-            //yet how do i get data from vehicle?
-            // do i create an empty vehicle instance with just license, and tell it to match with license in vehicle, return the whole record? 
-            //...
-            if (vehicle.checkExists(license){
-                //use that object
-                height = vehicle.getHeight();
-                length = vehicle.getLength();
-            }
-            else{
-                //prompts customer for attributes-- BUT THIS IS DONE BY UI RIGHT? 
-                //SO WE NEED THIS CHANGE IN user interface documnet, that after getting license, NOT ALWAYS WE ASK FOR ALL OTHER ATTRIBUTES
-                //need to push this instance to vehicle file too
-                vehicle.writeVehicle();
-            }
-
-            
-
-        };
-
-        void writeToFile(fstream& file) const // in-out: file - binary file stream
-        {
-        };
-
-        void readFromFile(fstream& file); // in-out: file - binary file stream 
-        {
-        };
-
-        
-        string toString() const // out: returns formatted string 
-        {
-            
-        };
-
-
-        void open(const string& sailingId, const string& license, const string& phone) // in: sailing ID, license, phone number        
-        {
+//************************************************************
+// Default Constructor
+// Initializes the reservation with default values
+//************************************************************
+Reservation::Reservation()
+{
+    sailingId ="";
     
-        };
+}
 
-        static int getTotalReservationsOnSailing(const string& sailingId)// in: sailingID
-        {
-    
-        };
+Reservation::Reservation(const string &sailingId, const string &license, const bool on_board) // in: sailingID, license, phone
+{
+    int length;
+    int height;
 
-        static bool removeReservation(const string& sailingId, const string& license) // in: sailingID, license
-        {
-    
-        };
+    if (vehicle.checkExists(license){
+        // use that object
+        height = vehicle.getHeight();
+        length = vehicle.getLength();
+        }
+        else{
+        // prompts customer for attributes-- BUT THIS IS DONE BY UI RIGHT?
+        // SO WE NEED THIS CHANGE IN user interface documnet, that after getting license, NOT ALWAYS WE ASK FOR ALL OTHER ATTRIBUTES
+        // need to push this instance to vehicle file too
+        vehicle.writeVehicle()
+        }
+};
 
-        static bool removeReservationsOnSailing(const std::string& sailingId) // in: sailingId
-        {
-    
-        };
+void Reservation::writeToFile(fstream &file) const // in-out: file - binary file stream
+{
+};
 
-        static bool checkExist(const string& sailingId, const string& license)// in: sailingID, license
-        {
-            
-    
-        };
+void Reservation::readFromFile(fstream &file); // in-out: file - binary file stream
+{};
 
-        static bool writeReservation(const string& sailingId, const string& license)// in: sailingID, license, phone                                                                                                                                                                                              {
-        {
-            
-    
-        };
+string Reservation::toString() const // out: returns formatted string
+{
 
-        static void setCheckedIn(const string& sailingId, const string& license) // in: sailingID, license
-        {
-    
-        };
+};
+
+void Reservation::open(const string &sailingId, const string &license, const string &phone) // in: sailing ID, license, phone number
+{
+
+};
+
+static int Reservation::getTotalReservationsOnSailing(const string &sailingId) // in: sailingID
+{
+
+};
+
+static bool Reservation::removeReservation(const string &sailingId, const string &license) // in: sailingID, license
+{
+
+};
+
+static bool Reservation::removeReservationsOnSailing(const std::string &sailingId) // in: sailingId
+{
+
+};
+
+static bool Reservation::checkExist(const string &sailingId, const string &license) // in: sailingID, license
+{
+
+};
+
+static bool Reservation::writeReservation(const string &sailingId, const string &license) // in: sailingID, license, phone                                                                                                                                                                                              {
+{
+
+};
+
+static void Reservation::setCheckedIn(const string &sailingId, const string &license) // in: sailingID, license
+{
 
 };
--- reservation.h
+++ reservation.h
@@ -1,3 +1,5 @@
+
+-------------
 //************************************************************
 // reservation.h
 //************************************************************
@@ -6,44 +8,36 @@
 // reservations.
 // July 10, 2025 Revision 1
 //************************************************************
-
 #pragma once
 
-#include <fstream>
 #include <string>
+#include <fstream>
+
 using namespace std;
 
-class Reservation {
-    public:
-        // Fixed lengths for binary record layout
-        static const int SAILING_ID_LENGTH = 15;
-        static const int LICENSE_LENGTH = 10;
-        static const int PHONE_LENGTH = 14;
-        static const int RECORD_SIZE = SAILING_ID_LENGTH + LICENSE_LENGTH + PHONE_LENGTH + 1;
+class Reservation
+{
 
-        // Reservation record fields
-        char sailingId[SAILING_ID_LENGTH + 1]; // +1 for null terminator
-        char license[LICENSE_LENGTH + 1]; // +1 for null terminator
-        char phone[PHONE_LENGTH + 1]; // +1 for null terminator
-        bool checkedIn; // Track whether the vehicle has checked in.
+public:
+    static const int SAILING_ID_LENGTH = 9;
+    static const int LICENSE_LENGTH = 10;
+    bool on_board;
+    static const int RECORD_SIZE = LICENSE_LENGTH + 1 + SAILING_ID_LENGTH + 1 + sizeof(bool);
 
-        //HOW CAN WE PROVIDE A DEFAULT CONSTRUCTOR HERE?
-        Reservation(); // Default Constructor 
-        Reservation(const string& sailingId, const string& license, const string& phone); // in: sailingID, license, phone
+    char sailingId[SAILING_ID_LENGTH + 1]; // +1 for null terminator
+    char license[LICENSE_LENGTH + 1];      // +1 for null terminator
 
-        void writeToFile(fstream& file) const; // in-out: file - binary file stream
-        void readFromFile(fstream& file); // in-out: file - binary file stream
-        
-        string toString() const; // out: returns formatted string
+    Reservation();                                                                     // Default Constructor                                                                   // Default Constructor
+    Reservation(const string &license, const string &sailingId, const bool &on_board); // in: sailingID, license, phone
 
-        void open(const string& sailingId, const string& license, const string& phone); // in: sailing ID, license, phone number
+    void writeToFile(fstream &file) const; // in-out: file - binary file stream
+    void readFromFile(fstream &file);      // in-out: file - binary file stream
 
-        static int getTotalReservationsOnSailing(const string& sailingId); // in: sailingID
-        static bool removeReservation(const string& sailingId, const string& license); // in: sailingID, license
-        static bool removeReservationsOnSailing(const string& sailingId); // in: sailingId
-        static bool checkExist(const string& sailingId, const string& license); // in: sailingID, license
-        static bool writeReservation(const string& sailingId, const string& license); // in: sailingID, license, phone
-        static void setCheckedIn(const string& sailingId, const string& license); // in: sailingID, license
-
-
+    static int getTotalReservationsOnSailing(const string &sailingId);             // in: sailingID
+    static bool removeReservation(const string &sailingId, const string &license); // in: sailingID, license
+    static bool removeReservationsOnSailing(const string &sailingId);              // in: sailingId
+    static bool checkExist(const string &sailingId, const string &license);        // in: sailingID, license
+    static bool writeReservation(const string &sailingId, const string &license);  // in: sailingID, license, phone
+    static void setCheckedIn(const string &sailingId, const string &license);      // in: sailingID, license
+    string toString() const;                                                       // out: returns formatted string
 };
